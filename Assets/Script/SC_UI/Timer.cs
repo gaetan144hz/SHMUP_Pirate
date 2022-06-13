aaa5ef18c210072e1451a86258a5f73e152a564a
Gaétan DUMAS <g.dumas@etudiant.ecolemelies.fr>
2022-06-13 10:23:52 +0200

    timer

---
+++ Assets/Script/SC_UI/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float sec;
+    public float min;
+    public float currentTime;
+    [SerializeField] float startingTime;
+
+    [SerializeField] TextMeshProUGUI countdownText;
+
+    void Awake()
+    {
+        sec = startingTime;
+    }
+
+    public void timeInstantiate()
+    {
+        StartCoroutine(timeCount());
+    }
+
+    IEnumerator timeCount()
+    {
+        sec = 0;
+        min = 0;
+        currentTime = 0;
+
+        while (true)
+        {
+            sec += 1;
+            currentTime += 1;
+            if (sec >= 60)
+            {
+                sec = 0;
+                min += 1;
+            }
+
+            if (sec <= 9)
+            {
+                countdownText.text = $"{min}:0{sec}";
+            }
+            else
+            {
+                countdownText.text = $"{min}:{sec}";
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
